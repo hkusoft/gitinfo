2b0a493350e9d19d746dbd05ded9caa81e914e6a
David <hkusoft@gmail.com>
2017-08-07 18:19:23 +0800

    Add LibGit2Sharp refence; add fetch tag function

---
+++ GitInfo/GitInfo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using LibGit2Sharp;
+using System.Linq;
+
+namespace GitInfo
+{
+    public class API
+    {
+		const string RepoPath = @"/Users/m1/Data/source/dch/mobility";
+
+		//"path\to\repo.git"
+		public static List<Tag> GetTags()
+		{
+
+			using (var repo = new Repository(RepoPath))
+			{
+				return repo.Tags.ToList<Tag>();
+			}
+
+		}
+    }
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var tags = API.GetTags();
+            foreach (var tag in tags)
+            {
+                Console.WriteLine(tag.FriendlyName);
+            }
+        }
+    }
+
+	
+}
